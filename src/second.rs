9af8a3f017b41c83c3ec6b4bbd08829cab8c4079
Maximilian R <maximus12793@gmail.com>
2023-01-27 01:18:40 +0100

    iter works

--- src/second.rs
+++ src/second.rs
@@ -1,6 +1,11 @@
 // Bad Stack implementation.
 //
 
+// Implement the following:
+// IntoIter -> T
+// IterMut  -> &mut T
+// Iter     -> &T
+
 // Tuple structs are an alternative form of struct,
 // useful for trivial wrappers around other types.
 pub struct IntoIter<T>(List<T>);
@@ -16,6 +21,27 @@ impl<T> Iterator for IntoIter<T> {
     fn next(&mut self) -> Option<Self::Item> {
         // Get fields of tuple struct by number
         self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_deref() }
+    }
+}
+
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref().map::<&Node<T>, _>(|node| &*node);
+            &node.elem
+        })
     }
 }
 
@@ -141,5 +167,18 @@ mod test {
         assert_eq!(iter.next(), Some(2));
         assert_eq!(iter.next(), Some(1));
         assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 }
